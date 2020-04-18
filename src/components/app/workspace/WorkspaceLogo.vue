235107187663564853e5defb8ef1212142b70991
nakhoa <nakhoa17@gmail.com>
2020-04-18 16:02:31 +0700

    workspace menus dropdown

--- src/components/app/workspace/WorkspaceLogo.vue
+++ src/components/app/workspace/WorkspaceLogo.vue
@@ -1,19 +1,45 @@
 <template>
-  <div class="d-flex w-logo">
-    <v-avatar color="teal" :size="size" tile style="border-radius: 7px" class="fill-height">
-      <v-img v-if="workspace.logo" :src="workspace.logo" />
-      <span v-else>
-        Logo
-      </span>
-    </v-avatar>
-    <div class="ml-2 flex-column">
-      <div class="font-weight-bold" style="font-size: 18px;">
-        {{ workspace.name }}
-      </div>
-      <div style="font-size: 13px;">
-        {{ user.username }}
-      </div>
-    </div>
+  <div class="d-flex">
+    <v-menu v-model="showMenu" offset-x>
+      <template v-slot:activator="{ on }">
+        <div class="workspace-logo" v-on="on">
+          <v-avatar
+            color="teal"
+            :size="size"
+            tile
+            style="border-radius: 7px"
+            class="fill-height"
+          >
+            <v-img v-if="workspace.logo" :src="workspace.logo" />
+            <span v-else>
+              logo
+            </span>
+          </v-avatar>
+          <div class="ml-2 flex-column">
+            <div class="font-weight-bold" style="font-size: 18px;">
+              {{ workspace.name }}
+              <v-btn icon x-small>
+                <v-icon x-small>fa-chevron-down</v-icon>
+              </v-btn>
+            </div>
+            <div style="font-size: .75em;">
+              {{ user.username }}
+            </div>
+          </div>
+        </div>
+      </template>
+
+      <v-list dense>
+        <v-list-item class="pa-0" v-for="(item, index) in items" :key="index" dense>
+          <v-btn text :to="item.path" class="text-capitalize w-full justify-start">
+            <template v-slot:default>
+              <v-icon small>{{ item.icon }}</v-icon>
+              <span class="ml-2" style="font-size: .75em; font-weight: bold">{{ item.title }}</span>
+            </template>
+          </v-btn>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </div>
 </template>
 
@@ -22,6 +48,15 @@ import { mapGetters } from "vuex";
 
 export default {
   name: "WorkspaceLogo",
+  data() {
+    return {
+      showMenu: false,
+      items: [
+        { title: "User profile", path: "settings/user", icon: "fa-user" },
+        { title: "Workspace setting", path: "settings/user", icon: "fa-landmark" },
+      ]
+    };
+  },
   props: {
     size: {
       type: [String, Number],
@@ -36,4 +71,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.workspace-logo {
+  cursor: pointer;
+  display: flex;
+}
 </style>
